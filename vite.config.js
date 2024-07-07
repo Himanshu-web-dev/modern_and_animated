412d217116c76d795cade6245ce29858a79531d0
Himanshu-web-dev <sarcasm1920@gmail.com>
2024-07-08 01:42:58 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/modern_animated_website/",
+  base: "/animated_landing_page/",
   plugins: [react()],
 });
