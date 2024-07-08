6cf9423c69d7de3889f1a928d1cc939e2249a9da
Himanshu-web-dev <sarcasm1920@gmail.com>
2024-07-08 10:03:49 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/animated_landing_page/",
+  base:"/modern_and_animated/"
   plugins: [react()],
 });
